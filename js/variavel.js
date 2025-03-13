1f55f09166d22d2fe4c1d57b8486a728fa86ea96
otavio-mmulato <otavio.mmulato@outlook.com.br>
2025-03-13 11:17:01 -0300

    js

---
+++ js/variavel.js
@@ -0,0 +1,11 @@
+function exemploVar(){
+    if(true){
+        //Definir variáveis
+        var x=10;
+        let y=20;
+        const z=30;
+    }
+    console.log(x); //acessível
+    console.log(y); //Erro: y não está definido
+    console.log(z); //Erro: z não está definido
+}
