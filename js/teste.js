f5fc8f55b45977e6b6a680a7f6306e70357c3b75
otavio-mmulato <otavio.mmulato@outlook.com.br>
2025-03-25 13:32:59 -0300

    ex16

---
+++ js/teste.js
@@ -0,0 +1,8 @@
+function somar(a, b, c, d) {
+    return a + b + c + d;
+}
+
+let resultado = somar(1, 1, 1, 1);
+
+// Exibir o resultado na tela
+document.getElementById("resultado").innerText = resultado;
