9122869ef0a361ec8164c810ae5fbff548bf2eb3
otavio-mmulato <otavio.mmulato@outlook.com.br>
2025-04-01 14:02:51 -0300

    função keydown

---
+++ js/dianapink.js
@@ -0,0 +1,5 @@
+const campo = document.getElementById("campoTexto");
+
+campo.addEventListener('keydown', function(event){
+    console.log('Tecla pressionada:', event.key); //alert(event.key);
+});
