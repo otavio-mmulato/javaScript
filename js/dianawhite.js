23440b9be89fae49f9b47fca53e31008307e20ad
otavio-mmulato <otavio.mmulato@outlook.com.br>
2025-04-01 15:20:22 -0300

    versao on

---
+++ js/dianawhite.js
@@ -0,0 +1,6 @@
+function mudarCor(){
+    document.getElementById("meuDiv").style.backgroundColor = "lightgreen";
+}
+function mudarCorBase(){
+    document.getElementById("meuDiv").style.backgroundColor = "lightblue";
+}
